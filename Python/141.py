371883ef96725584f312674aac145604d9f5fb58
liyao <nudtliyao@foxmail.com>
2021-11-12 21:34:55 +0800

    开始学习数组和链表

---
+++ Python/141.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+class ListNode:
+	def __init__(self, x):		
+		self.val = x
+		self.next = None
+		
+		
+
+	
+class Solution:
+	def hasCycle(self, head: ListNode) -> bool:
+		return None
+
+first = LinkList()
+first = LinkList.initlist_tail(first, [1, 2, 3])
+#print(first.length)
+
+l1_0 = ListNode(0)
+l1_2 = ListNode(2)
+l2_0 = ListNode(1)
+l2_2 = ListNode(4)
+
+l1_0.next = l1_2
+l2_0.next = l2_2
+
+
+	
